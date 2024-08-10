869d0508950113a624a1012d66441737bd5a1c98
Turdugul <okonbaevaturdugul@gmail.com>
2024-08-10 14:25:09 +0100

    add filter component

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -42,7 +42,8 @@ export const getCabins = async function () {
     .from('cabins')
     .select('id, name, maxCapacity, regularPrice, discount, image')
     .order('name');
- 
+ // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
   if (error) {
     console.error(error);
     throw new Error('Cabins could not be loaded');
